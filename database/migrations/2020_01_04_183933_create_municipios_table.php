a49d3772980cf55d62e4eb86e9f8ec2880573c88
mauricio-testa <iciotesta@hotmail.com>
2020-01-04 16:52:20 -0300

    municipio model, migration

--- database/migrations/2020_01_04_183933_create_municipios_table.php
+++ database/migrations/2020_01_04_183933_create_municipios_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateMunicipiosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('email');
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('municipios', function (Blueprint $table) {
+            $table->integer('codigo')->nullable(false);
+            $table->string('nome', 240)->nullable(false);
+            $table->char('uf', 2)->nullable(false);
+            $table->primary('codigo');
         });
     }
 
@@ -28,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('municipios');
     }
 }
