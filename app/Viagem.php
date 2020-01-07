a8fdf4b29d00f43dcd168664bb20ef33b7272ebb
mauricio-testa <iciotesta@hotmail.com>
2020-01-07 10:19:45 -0300

    ident

--- app/Viagem.php
+++ app/Viagem.php
@@ -8,17 +8,18 @@ class Viagem extends Model
 {
     public static function getViagemList() {
         return self::select('viagems.*', 
-                'mu.nome as municipio_nome', 
-                'mo.nome as motorista_nome', 
-                've.descricao as veiculo_nome', 
-                've.placa as veiculo_placa',
-                'mu.nome as municipio_nome'
+                'mu.nome        as municipio_nome', 
+                'mo.nome        as motorista_nome', 
+                've.descricao   as veiculo_nome', 
+                've.placa       as veiculo_placa',
+                'mu.nome        as municipio_nome'
             )
-            ->leftJoin('municipios as mu', 'viagems.cod_destino', '=', 'mu.codigo')
-            ->leftJoin('unidades as un', 'viagems.id_unidade', '=', 'un.id')
-            ->leftJoin('motoristas as mo', 'viagems.id_motorista', '=', 'mo.id')
-            ->leftJoin('veiculos as ve', 'viagems.id_veiculo', '=', 've.id')
-            ->where('viagems.id_unidade', '=', 1)
+            ->leftJoin('municipios as mu'   , 'viagems.cod_destino',    '=', 'mu.codigo')
+            ->leftJoin('unidades as un'     , 'viagems.id_unidade',     '=', 'un.id')
+            ->leftJoin('motoristas as mo'   , 'viagems.id_motorista',   '=', 'mo.id')
+            ->leftJoin('veiculos as ve'     , 'viagems.id_veiculo',     '=', 've.id')
+            
+            ->where('viagems.id_unidade'    , '=', 1)
             ->paginate(config('constants.default_pagination_size'));
     }
 }
