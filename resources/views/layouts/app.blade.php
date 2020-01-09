ebf9e72ee15171ac25db9009c9c6877e8fa2cf5f
mauricio-testa <iciotesta@hotmail.com>
2020-01-09 09:53:35 -0300

    change menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,13 +52,14 @@
                             <li><a href="{{ url('/cadastros/veiculos') }}">Veículos</a></li>
                             <li><a href="{{ url('/cadastros/pacientes') }}">Pacientes</a></li>
                             <li><a href="{{ url('/viagens') }}">Viagens</a>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+                                    <li><a href="{{ route('register') }}">Novo usuário</a></li>
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
