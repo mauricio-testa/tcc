588f155a0ca5aa4ce10d3281422841d6705bdc75
mauricio-testa <iciotesta@hotmail.com>
2020-02-06 17:03:13 -0300

    adjust routes

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,14 @@
 |
 */
 
+
 Route::middleware(['auth'])->group(function () {
 
     Route::get('/',         'IndexController@index')->name('index');
     Route::get('/viagens',  'ViagemController@index');
 
     Route::group(['prefix' => 'cadastros'], function () {
-
-        Route::group(['prefix' => 'motoristas'], function () {
-            Route::get('/',             'MotoristaController@index')->name('motorista.index');
-            Route::get('/edit/{id?}',   'MotoristaController@edit')->name('motorista.edit');
-            Route::get('/delete/{id}',  'MotoristaController@delete')->name('motorista.delete');
-        });
-        
-        Route::get('/veiculos',     'VeiculoController@index');
-        Route::get('/pacientes',    'PacienteController@index');
+        Route::view('/motoristas', 'motorista');
     });
 
 });
