4546d704281a1a96188df8b9bce9aa2cf79be4a1
mauricio-testa <iciotesta@hotmail.com>
2020-03-30 23:50:35 -0300

    insert update viagem

--- app/Http/Controllers/Api/ViagemController.php
+++ app/Http/Controllers/Api/ViagemController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Viagem;
 use App\Http\Controllers\Helpers\ErrorInterpreter;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 
 class ViagemController extends Controller
 {
@@ -22,24 +23,32 @@ class ViagemController extends Controller
         }  
     }
 
-    public function create()
-    {
-        //
-    }
-
     public function store(Request $request)
     {
-        //
+        try {
+            $viagem = $request->all();
+            $viagem['id_unidade'] = Auth::user()->id_unidade;
+            $last_id = Viagem::insertGetId($viagem);
+            return response()->json(['id' => $last_id]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => ErrorInterpreter::getMessage($th)
+            ]);
+        }
     }
 
-    public function show($id)
-    {
-        //
-    }
 
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $viagem = Viagem::findOrFail($id);
+            $viagem->updated_at = Carbon::now();
+            $viagem->update($request->all());
+        } catch (\Throwable $th) {
+            return response()->json([
+                'error' => ErrorInterpreter::getMessage($th)
+            ]);
+        }
     }
 
     public function destroy($id)
