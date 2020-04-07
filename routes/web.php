71fb424cba623250d78c821d1009b935921e98d8
mauricio-testa <iciotesta@hotmail.com>
2020-04-07 19:57:14 -0300

    add middleware admin

--- routes/web.php
+++ routes/web.php
@@ -10,37 +10,35 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\Http\Middleware\Admin;
 
 Route::middleware(['auth'])->group(function () {
 
-    Route::get('/',         'IndexController@index')->name('index');
+    Route::get('/', 'IndexController@index')->name('index');
     Route::view('/viagens', 'viagem');
 
     Route::group(['prefix' => 'cadastros'], function () {
-        Route::view('/motoristas'   , 'motorista');
-        Route::view('/veiculos'     , 'veiculo');
-        Route::view('/pacientes'    , 'paciente');
-    });
-
-    Route::group(['prefix' => 'admin'], function () {
-        Route::view('/unidades'     , 'admin.unidade');
+        Route::view('/motoristas', 'motorista');
+        Route::view('/veiculos', 'veiculo');
+        Route::view('/pacientes', 'paciente');
     });
 
     Route::prefix('api')->group(function () {
-        Route::resource('motoristas'    , 'Api\MotoristaController');
-        Route::resource('veiculos'      , 'Api\VeiculoController');
-        Route::resource('pacientes'     , 'Api\PacienteController');
-        Route::resource('viagens'       , 'Api\ViagemController');
-        Route::resource('lista'         , 'Api\ListaController');
-        Route::resource('municipios'    , 'Api\MunicipioController');
-        Route::resource('unidades'      , 'Api\UnidadeController');
-        Route::resource('usuarios'      , 'Api\UsuarioController');
+        Route::resource('motoristas', 'Api\MotoristaController');
+        Route::resource('veiculos', 'Api\VeiculoController');
+        Route::resource('pacientes', 'Api\PacienteController');
+        Route::resource('viagens', 'Api\ViagemController');
+        Route::resource('lista', 'Api\ListaController');
+        Route::resource('municipios', 'Api\MunicipioController');
+        Route::resource('unidades', 'Api\UnidadeController')->middleware(Admin::class);
+        Route::resource('usuarios', 'Api\UsuarioController')->middleware(Admin::class);
     });
 
     Route::prefix('relatorios')->group(function () {
-        Route::get('lista/{viagem}'     , 'Report\ListaController@index')->where('viagem', '[0-9]+');;
-    });    
+        Route::get('lista/{viagem}', 'Report\ListaController@index')->where('viagem', '[0-9]+');;
+    });
+
+    Route::view('admin/unidades', 'admin.unidade')->middleware(Admin::class);
 
 });
 
