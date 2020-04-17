b84e51b4b8bd2eea4b82a553c503dbe9716036c2
mauricio-testa <iciotesta@hotmail.com>
2020-04-16 23:07:54 -0300

    organize index dashboard

--- resources/assets/js/components/dashboard/Index.vue
+++ resources/assets/js/components/dashboard/Index.vue
@@ -2,13 +2,36 @@
   <div>
     <v-sheet class="headline my-2">Dashboard</v-sheet>
 
+    <!-- estatisticas -->
     <dashboard-stats></dashboard-stats>
-    <v-sheet class="subtitle-1 my-4">Transporte de pacientes por período</v-sheet>
-    <dashboard-graphic></dashboard-graphic>
-    <v-sheet class="subtitle-1 my-4">Top 5</v-sheet>
-    <dashboard-lists></dashboard-lists>
+
+    <v-row>
+      <!--gratico pacientes transportados -->
+      <v-col sm="12" md="6">
+        <v-sheet class="subtitle-1 my-4">Transporte de Pacientes por Mês nos Últimos 6 Mêses</v-sheet>
+          <dashboard-graph-pacientes></dashboard-graph-pacientes>
+      </v-col>
+
+      <v-col sm="12" md="6">
+
+        <!-- listagem proximas viagens -->
+        <v-sheet class="subtitle-1 my-4">Proximas Viagens</v-sheet>
+        <dashboard-next-viagens></dashboard-next-viagens>
+
+        <!-- grafico viagens por mes -->
+        <v-sheet class="subtitle-1 my-4">Viagens por Mês Neste Ano</v-sheet>
+        <dashboard-graph-viagens></dashboard-graph-viagens>
+      </v-col>
+    </v-row>
+
+    <!-- top 5 veiculos, motoristas, destinos -->
+    <dashboard-top5></dashboard-top5>
+
   </div>
 </template>
 
 <script>
+export default {
+  props: ['data'],
+}
 </script>
