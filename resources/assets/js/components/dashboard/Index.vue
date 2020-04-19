c9d7d3a9dc52137609010ebdd3d6255b2912eb17
mauricio-testa <iciotesta@hotmail.com>
2020-04-19 17:56:49 -0300

    welcome!

--- resources/assets/js/components/dashboard/Index.vue
+++ resources/assets/js/components/dashboard/Index.vue
@@ -1,49 +1,72 @@
 <template>
   <div>
-    <v-sheet class="headline my-2">Dashboard</v-sheet>
 
+    <v-sheet class="headline my-2">Dashboard</v-sheet>
+    
     <!-- estatisticas -->
     <dashboard-stats></dashboard-stats>
 
-    <v-row>
-      <!--gratico pacientes transportados -->
-      <v-col sm="12" md="6">
-        <v-sheet class="subtitle-1 my-4">Transporte de Pacientes por Mês nos Últimos 6 Mêses</v-sheet>
-        <v-card>
-          <v-card-text>
-            <dashboard-graph-pacientes :data="data.total_passageiros_mes" style="height: 335px"></dashboard-graph-pacientes>
-          </v-card-text>
-        </v-card>
-        <v-sheet class="subtitle-1 my-4">Índice de Absenteísmo</v-sheet>
-        <v-card>
-          <v-card-text>
-            Lorem Ipsum
-          </v-card-text>
-        </v-card>
-      </v-col>
+    <!-- welcome -->
+    <div v-if="newUser">
+      <v-alert outlined color="blue" class="mt-3">
+        Olá! Estamos muito felizes em saber que você está utilizando o sistema SIMTRAP :)
+      </v-alert>
+      <v-alert outlined color="blue">
+        Nós preparamos para você um dashboard com várias informações e estatísticas muito interessantes! <br>
+        Porém, ele somente será exibido por completo quando você adicionar uma viagem e, pelo menos, um passageiro nela. 
+        Não se esqueça de antes realizar o cadastro dos veículos, motoristas e pacientes! - Utilize os menus da esquerda para acessar eles.<br>
+      </v-alert>
+      <v-btn block color="secondary" dark href="./viagens">Adicionar a Primeira Viagem</v-btn>
+    </div>
 
-      <v-col sm="12" md="6">
-        <!-- listagem proximas viagens -->
-        <v-sheet class="subtitle-1 my-4">Proximas Viagens</v-sheet>
-        <dashboard-next-viagens></dashboard-next-viagens>
+    <div v-else>
 
-        <!-- grafico viagens por mes -->
-        <v-sheet class="subtitle-1 my-4">Viagens por Mês Neste Ano</v-sheet>
-        <v-card>
-          <v-card-text>
-            <dashboard-graph-viagens :data="data.total_viagens_mes" style="height: 168px"></dashboard-graph-viagens>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+      <v-row>
+        <!--gratico pacientes transportados -->
+        <v-col sm="12" md="6">
+          <v-sheet class="subtitle-1 my-4">Transporte de Pacientes por Mês nos Últimos 6 Mêses</v-sheet>
+          <v-card>
+            <v-card-text>
+              <dashboard-graph-pacientes :data="data.total_passageiros_mes" style="height: 335px"></dashboard-graph-pacientes>
+            </v-card-text>
+          </v-card>
+          <v-sheet class="subtitle-1 my-4">Índice de Absenteísmo</v-sheet>
+          <v-card>
+            <v-card-text>
+              Lorem Ipsum
+            </v-card-text>
+          </v-card>
+        </v-col>
 
-    <!-- top 5 veiculos, motoristas, destinos -->
-    <dashboard-top5></dashboard-top5>
+        <v-col sm="12" md="6">
+          <!-- listagem proximas viagens -->
+          <v-sheet class="subtitle-1 my-4">Proximas Viagens</v-sheet>
+          <dashboard-next-viagens></dashboard-next-viagens>
+
+          <!-- grafico viagens por mes -->
+          <v-sheet class="subtitle-1 my-4">Viagens por Mês Neste Ano</v-sheet>
+          <v-card>
+            <v-card-text>
+              <dashboard-graph-viagens :data="data.total_viagens_mes" style="height: 168px"></dashboard-graph-viagens>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
+      <!-- top 5 veiculos, motoristas, destinos -->
+      <dashboard-top5></dashboard-top5>
+
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["data"]
+  props: ["data"],
+  computed: {
+    newUser() {
+      return this.data.statistics.total_pacientes_transportados == 0 || this.data.statistics.total_viagens == 0;
+    }
+  }
 };
 </script>
