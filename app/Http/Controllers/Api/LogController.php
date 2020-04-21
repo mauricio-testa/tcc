dfc0e33da771500e7e45f551deb565935042836e
mauricio-testa <iciotesta@hotmail.com>
2020-04-20 21:58:20 -0300

    finish logs

--- app/Http/Controllers/Api/LogController.php
+++ app/Http/Controllers/Api/LogController.php
@@ -5,15 +5,31 @@ namespace App\Http\Controllers\Api;
 use App\Log;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class LogController extends Controller
 {
-    public function index(Request $request)
+    public function index (Request $request)
     {
-        $query = Log::whereNotNull('id');
+        $query = DB::table('logs')
+            ->select('logs.*', 'users.name')
+            ->leftJoin('users', 'logs.id_user','=','users.id')
+            ->orderBy('created_at', 'DESC');
 
-        //if(!empty($request->search))
-        //$query->where('descricao', 'like', '%'.$request->search.'%');
+        if ('null' != $request->level)
+        $query->where('logs.level', $request->level);
+
+        if ('null' != $request->action)
+        $query->where('logs.action', $request->action);
+
+        if ($request->id_user > 0)
+        $query->where('logs.id_user', $request->id_user);
+
+        if ('null' != $request->context)
+        $query->where('logs.context', $request->context);
+
+        if ($request->context_id > 0)
+        $query->where('logs.context_id', $request->context_id);
 
         return $query->paginate(config('constants.PAGINATION_SIZE'));
     }
--- resources/assets/js/components/admin/Logs.vue
+++ resources/assets/js/components/admin/Logs.vue
@@ -1,30 +1,29 @@
 <template>
     <div>
         <v-card class="ma-md-2 px-2 ma-xs-0">
+
+            <!-- table header -->
+            <v-card-title>
+                <v-row>
+                <v-col cols="12" md="4">
+                    Logs
+                </v-col>
+                <v-col cols="12" md="8">
+                    <v-form @submit.prevent="search()" class="d-flex">
+                    <v-select class="mr-2" :items="filters.context" v-model="get.context" label="Context"></v-select>
+                    <v-text-field class="mr-2" type="number" v-model="get.context_id" label="ID"></v-text-field>
+                    <v-select class="mr-2" :items="filters.level" v-model="get.level" label="Level"></v-select>
+                    <v-select class="mr-2" :items="filters.action" v-model="get.action" label="Action"></v-select>
+                    <v-text-field class="mr-2" type="number" v-model="get.id_user" label="User"></v-text-field>
+                    <v-icon class="mr-2" @click="search()">mdi-magnify</v-icon>
+                    <v-icon class="mr-2" @click="resetSearch()">mdi-close</v-icon>
+                    </v-form>
+                </v-col>
+                </v-row>                        
+            </v-card-title>
+
             <v-row>
                 <v-col cols="8">
-                    <!-- table header -->
-                    <v-card-title>
-                        <v-row>
-                        <v-col cols="12" md="8">
-                            Logs
-                        </v-col>
-                        <v-col cols="12" md="4">
-                            <v-form @submit.prevent="search()">
-                            <v-text-field 
-                                v-model="searchWord" 
-                                :prepend-icon="'mdi-magnify'"
-                                @click:clear="resetSearch()"
-                                :hint="'Digite sua busca e tecle Enter!'"
-                                label="Pesquisar"
-                                clearable
-                                >
-                            </v-text-field>
-                            </v-form>
-                        </v-col>
-                        </v-row>                        
-                    </v-card-title>
-
                     <!-- table  -->
                     <v-data-table
                         :headers="headers"
@@ -35,6 +34,17 @@
                         no-data-text="Nenhum dado encontrado."
                         loading-text="Buscando dados..."
                     >
+                        <template v-slot:item.action="{ item }">
+                            <v-chip dark small :class="getChipClass(item.action)">{{item.action}}</v-chip>
+                        </template>
+
+                        <template v-slot:item.level="{ item }">
+                            <v-chip dark small :class="getChipClass(item.level)">{{item.level}}</v-chip>
+                        </template>
+                        
+                        <template v-slot:item.see="{ item }">
+                            <v-icon outlined @click="explanation(item)" :class="{'blue--text': item.id == selectedItem.id}">mdi-eye</v-icon>
+                        </template>
                     </v-data-table>
 
                     <div class="text-center py-3">
@@ -47,7 +57,21 @@
                     </div>
                 </v-col>
                 <v-col cols="4">
-                    View
+                    <span class="title d-block text-capitalize"> {{selectedItem.context}} #{{selectedItem.context_id}}</span>
+                    <span class="caption text--secondary">{{ selectedItem.created_at }}</span>
+                    <v-divider></v-divider>
+                    <v-chip-group>
+                        <v-chip dark small :class="getChipClass(selectedItem.level)">{{selectedItem.level}}</v-chip>
+                        <v-chip dark small :class="getChipClass(selectedItem.action)">{{selectedItem.action}}</v-chip>
+                        <v-chip dark small>{{selectedItem.name}}</v-chip>
+                        <v-chip dark small>Unidade {{selectedItem.id_unidade}}</v-chip>
+                    </v-chip-group>
+                    <v-divider></v-divider>
+                    <div class="subtitle-2 my-3"> {{ selectedItem.message }} </div>
+                    <code class="pa-4">
+                        <span>{{selectedItem.payload | format}}</span>
+                    </code>
+                    
                 </v-col>
             </v-row>
         
@@ -64,16 +88,42 @@
             // main data
             logs: [],
             api: window.__routes.api.log,
+            selectedItem: {},
+            selectedPayload: {},
 
             loading: {
                 list: false,
             },
 
+            filters: {
+                context: ['lista', 'viagens'],
+                level: ['CRUD', 'INFO', 'WARNING', 'EXCEPTION'],
+                action: ['INSERT', 'UPDATE', 'DELETE', 'EXPORT'],
+            },
+            get: {
+                context: null,
+                context_id: null,
+                level: null,
+                action: null,
+                id_user: null
+            },
+            defaultGet: {
+                context: null,
+                context_id: null,
+                level: null,
+                action: null,
+                id_user: null
+            },
+
             // table column names
             headers: [
                 { text: '#', value: 'id'},
+                { text: 'Contexto', value: 'context'},
                 { text: 'Level', value: 'level'},
+                { text: 'Ação', value: 'action'},
+                { text: 'Usuário', value: 'name'},
                 { text: 'Mensagem', value: 'message'},
+                { text: 'Ver', value: 'see'},
             ],
 
             // server side pagination
@@ -84,7 +134,6 @@
                 perPage: 0,
             },
 
-            searchWord: null,
         }),
 
         mounted() {
@@ -101,7 +150,11 @@
                 let page      = vm.pagination.current;
                 let urlFetch  = this.api+'?page='+page;
 
-                if(vm.searchWord != null && vm.searchWord != '') urlFetch+= '&search='+vm.searchWord;
+                urlFetch+= '&context='+vm.get.context;
+                urlFetch+= '&context_id='+vm.get.context_id;
+                urlFetch+= '&level='+vm.get.level;
+                urlFetch+= '&action='+vm.get.action;
+                urlFetch+= '&id_user='+vm.get.id_user;
 
                 vm.loading.list = true;
 
@@ -113,12 +166,28 @@
                         vm.pagination.total       = response.data.last_page;
                         vm.pagination.totalItems  = response.data.total;
                         vm.pagination.perPage     = response.data.per_page;
-                        
+                        if (vm.logs.length > 0) {
+                            vm.selectedItem = vm.logs[0]
+                        }
                     })
                     .catch(function(error) {
                         vm.$toast.error('Erro ao buscar logs')
                     })
                     .finally(() => { vm.loading.list = false; });
+            },
+
+            getChipClass (label) {
+                if (label == 'INSERT') return 'teal';
+                if (label == 'UPDATE') return 'light-blue';
+                if (label == 'DELETE') return 'deep-orange';
+                if (label == 'CRUD') return 'blue-grey darken-1';
+                if (label == 'EXCEPTION') return 'deep-orange';
+                if (label == 'INFO') return 'light-blue';
+                if (label == 'WARNING') return 'orange';
+            },
+
+            explanation (item) {
+                this.selectedItem = item
             },
 
             // Inicia uma pesquisa
@@ -130,7 +199,7 @@
             // Limpa a pesquisa
             resetSearch: function () {
                 this.pagination.current = 1;
-                this.searchWord = null;
+                this.get = Object.assign({}, this.defaultGet);
                 this.getItems();
             },
 
@@ -138,6 +207,15 @@
             onPageChange: function () {
                 this.getItems();
             },
+        },
+        filters: {
+            format (json) {
+                if (!json) return;
+                let openKey = json.replace(/{/g,'{\n   ');
+                let closeKey = openKey.replace(/}/g,'\n  }');
+                let wrap = closeKey.replace(/,/g,', \n  ');
+                return wrap;
+            }
         }
     }
 </script>
